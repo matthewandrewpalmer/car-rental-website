723ec9d95d8db494ca6d7dde946b187af5689f86
Matthew Palmer <matthewandrewpalmer@outlook.com>
2019-07-17 16:02:35 +0100

    Updates

---
+++ precache-manifest.38b5f52154c5ed63c29b010c2887078c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bdcf2724091473e275b999812a34c93",
+    "url": "/car-rental-website/index.html"
+  },
+  {
+    "revision": "d65d926aef9f5fcce55b",
+    "url": "/car-rental-website/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/car-rental-website/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d65d926aef9f5fcce55b",
+    "url": "/car-rental-website/static/js/main.af1f1671.chunk.js"
+  },
+  {
+    "revision": "3ff55d17818c82a72517",
+    "url": "/car-rental-website/static/js/runtime~main.2596897a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/car-rental-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
